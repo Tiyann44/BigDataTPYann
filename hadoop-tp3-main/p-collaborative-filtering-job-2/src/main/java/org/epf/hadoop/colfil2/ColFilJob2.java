4fed6de55940b2ed4ac460e155f0710a91163b5a
Tiyann44 <tiyann2410@gmail.com>
2025-01-16 22:37:45 +0100

    Job2 réussi du premier coup (ç'est sûr y'a une erreur quelque part c'est pas possible)

--- hadoop-tp3-main/p-collaborative-filtering-job-2/src/main/java/org/epf/hadoop/colfil2/ColFilJob2.java
+++ hadoop-tp3-main/p-collaborative-filtering-job-2/src/main/java/org/epf/hadoop/colfil2/ColFilJob2.java
@@ -1,7 +1,38 @@
 package org.epf.hadoop.colfil2;
 
-public class ColFilJob2 {
-    public static void main(String[]args){
-        System.out.println("Your code here");
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class ColFilJob2{
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "Job 2: Common Relations");
+
+        job.setJarByClass(ColFilJob2.class);
+
+        // Mapper and Reducer
+        job.setMapperClass(CommonRelationsMapper.class);
+        job.setReducerClass(CommonRelationsReducer.class);
+
+        // Custom Partitioner
+        job.setPartitionerClass(UserPairPartitioner.class);
+
+        // Outputs
+        job.setMapOutputKeyClass(UserPair.class);
+        job.setMapOutputValueClass(Text.class);
+        job.setOutputKeyClass(UserPair.class);
+        job.setOutputValueClass(Text.class);
+
+        // Number of reducers
+        job.setNumReduceTasks(2);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
